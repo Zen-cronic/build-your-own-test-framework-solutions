2623dcb17cb3433ac925eb3cc2774a5402e4cdea
Zen-cronic <83657429+Zen-cronic@users.noreply.github.com>
2024-06-26 18:43:15 -0400

    end: ch3

--- concise-test/src/runner.mjs
+++ concise-test/src/runner.mjs
@@ -4,60 +4,103 @@ import { color } from "./colors.mjs";
 
 // [ERR_UNSUPPORTED_ESM_URL_SCHEME]: Only URLs with a scheme in: file, data, and node are supported by the default ESM loader. On Windows, absolute paths must be valid file:// URLs. Received protocol 'c:'
 
-let anyFailure = false;  //module-level vari
+// let anyFailure = false;  //module-level vari
 
-let successes = 0;
-
-let failures = 0;
+const TICK = "\u2713";
+const CROSS = "\u2717";
 
 const exitCodes = {
   ok: 0,
   failures: 1,
 };
+
+let successes = 0;
+
+// let currentDescribe ;
+let describeStack = [];
+
+// let failures = 0;
+const failures = [];
+
+const printFailure = (failure) => {
+  console.error(color(composeTestDescription(failure)));
+  console.error(failure.error);
+  console.error("");
+};
+const printAllFailures = () => {
+  if (failures.length > 0) {
+    console.error("");
+    console.error("Failures:");
+    console.error("");
+
+    failures.forEach(printFailure);
+  }
+};
+
+const composeTestDescription = ({ name, describeStack }) => {
+  return [...describeStack, name]
+    .map((describeName) => `<bold><red>${describeName}</red></bold>`)
+    .join(" -> ");
+};
+
+const indent = (message) => {
+  return `${" ".repeat(describeStack.length * 2)}${message}`;
+};
+
+const withoutLast = (arr) => arr.slice(0, -1);
+
 export const run = async () => {
   try {
     const p = path.resolve(process.cwd(), "test/tests.mjs");
-    // console.log({p});
-
-    const f = p.replace(/^C/, "file"); //wrong, doesn't encode posix, etc
-    // console.log({f});
-
-    // await import(f)
-    // await import(p)
 
     const windowsPath = pathToFileURL(p);
     // console.log({windowsPath});
 
     await import(windowsPath);
-    // const v = path.dirname(fileURLToPath(import.meta.url))
-    // console.log({v});
   } catch (error) {
     console.error(error);
   }
 
+  printAllFailures();
+  console.log();
   console.log(
-    color(`<green>${successes}</green> tests have passed\n<red>${failures}</red> tests have failed
+    color(`<green>${successes}</green> tests have passed, <red>${failures.length}</red> tests have failed
     `)
   );
   console.log("\nJello");
-//   process.exit(anyFailure ? exitCodes.failures : exitCodes.ok);
-  process.exit(failures != 0 ? exitCodes.failures: exitCodes.ok)
-
+  //   process.exit(anyFailure ? exitCodes.failures : exitCodes.ok);
+  process.exit(failures.length != 0 ? exitCodes.failures : exitCodes.ok);
 };
 
 export const it = (name, body) => {
   try {
     body();
+    console.log(
+      indent(color(`<bold><green>${TICK} PASS ${name}</green></bold>`))
+    );
     successes++;
   } catch (e) {
-    console.error(color(`<bold><red>${name}</red></bold>`));
-    // console.error(e);
+    console.log(indent(color(`<bold><red>${CROSS} PASS ${name}</red></bold>`)));
     // anyFailure = true
-    failures++;
+    // failures.push(e);
+    failures.push({
+      error: e,
+      name: name,
+      // describeName: currentDescribe
+      describeStack,
+    });
   }
 
- 
   //prog exits on first failure - include in run, not it
   // process.exit(anyFailure ? exitCodes.failures: exitCodes.ok)
+};
 
+export const describe = (name, body) => {
+  console.log(indent(name));
+  // console.log(name);
+  // currentDescribe = name
+  describeStack = [...describeStack, name];
+  body(); //the it invoked
+  // currentDescribe = undefined
+  describeStack = withoutLast(describeStack);
 };
--- todo-example/test/tests.mjs
+++ todo-example/test/tests.mjs
@@ -1,76 +1,85 @@
-import { it } from "concise-test";
+import { it, describe } from "concise-test";
 import { emptyTodo, markAsDone } from "../src/todo.mjs";
 import { TodoRepository } from "../src/todoRepository.mjs";
 // const repository = new TodoRepository() // global
 
-const mockThrow = () => {throw new Error("Jello, Error")}
+const mockThrow = () => {
+  throw new Error("Jello, Error");
+};
 
-it("should set completedAt when calling markAsDone", () => {
+describe("todo", () => {
+  it("should set completedAt when calling markAsDone", () => {
+    mockThrow();
+    const todo = emptyTodo();
 
-  mockThrow()
-  const todo = emptyTodo();
-
-  if (!markAsDone(todo).completedAt)
-    throw new Error(
-      "completedAt not set when calling markAsDone"
-    );
-});
-
-it("should throw an exception when adding a todo without a title", () => {
-  mockThrow()
-  const repository = new TodoRepository();
-
-  try {
-    repository.add(emptyTodo());
-    throw new Error(
-      "no error thrown when adding an empty todo"
-    );
-  } catch (e) {
-    if (e.message !== "title cannot be blank")
+    if (!markAsDone(todo).completedAt)
       throw new Error(
-        "wrong message in guard clause when adding an empty todo"
+        "completedAt not set when calling markAsDone"
       );
-  }
+  });
 });
 
-it("should throw errors when adding a repeated todo", () => {
-  const repository = new TodoRepository(); // global
+describe("ToDoRepository", () => {
+  describe("add method", () => {
+    it("should throw an exception when adding a todo without a title", () => {
+      mockThrow();
+      const repository = new TodoRepository();
 
-  const newTodo = { ...emptyTodo(), title: "test" };
-  repository.add(newTodo);
+      try {
+        repository.add(emptyTodo());
+        throw new Error(
+          "no error thrown when adding an empty todo"
+        );
+      } catch (e) {
+        if (e.message !== "title cannot be blank")
+          throw new Error(
+            "wrong message in guard clause when adding an empty todo"
+          );
+      }
+    });
 
-  const repeatedTodo = { ...newTodo };
-  try {
-    repository.add(repeatedTodo);
-    throw new Error(
-      "no error thrown when adding a repeated todo"
-    );
-  } catch (e) {
-    if (e.message !== "todo already exists")
-      throw new Error(
-        "wrong message in guard clause when adding an existing todo"
-      );
-  }
-});
+    it("should throw errors when adding a repeated todo", () => {
+      const repository = new TodoRepository(); // global
 
-it("finds an added todo", () => {
-  const repository = new TodoRepository();
-  const newTodo = { ...emptyTodo(), title: "test" };
-  repository.add(newTodo);
+      const newTodo = { ...emptyTodo(), title: "test" };
+      repository.add(newTodo);
 
-  if (repository.findAllMatching("").length !== 1)
-    throw new Error("added todo was not returned");
-});
+      const repeatedTodo = { ...newTodo };
+      try {
+        repository.add(repeatedTodo);
+        throw new Error(
+          "no error thrown when adding a repeated todo"
+        );
+      } catch (e) {
+        if (e.message !== "todo already exists")
+          throw new Error(
+            "wrong message in guard clause when adding an existing todo"
+          );
+      }
+    });
+  });
 
-it("filters out todos that do not match filter", () => {
-  const repository = new TodoRepository();
-  const newTodo = { ...emptyTodo(), title: "test" };
-  repository.add(newTodo);
-  if (
-    repository.findAllMatching("some other test")
-      .length !== 0
-  )
-    throw new Error(
-      "filter was not applied when finding matches"
-    );
+  describe("findAllMatching method", () => {
+    it("finds an added todo", () => {
+      const repository = new TodoRepository();
+      const newTodo = { ...emptyTodo(), title: "test" };
+      repository.add(newTodo);
+
+      if (repository.findAllMatching("").length !== 1)
+        throw new Error("added todo was not returned");
+    });
+
+    it("filters out todos that do not match filter", () => {
+      const repository = new TodoRepository();
+      const newTodo = { ...emptyTodo(), title: "test" };
+      repository.add(newTodo);
+      if (
+        repository.findAllMatching("some other test")
+          .length !== 0
+      )
+        throw new Error(
+          "filter was not applied when finding matches"
+        );
+    });
+  });
 });
